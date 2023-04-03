a243f4d5dcc8ae1687e086c5bfa3dbf7f617641e
hhtetwei <hhtet263510@gmail.com>
2023-04-03 10:59:34 +0630

    check accessToken before component mount

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -1,8 +1,19 @@
 import React from "react";
 
 import DrawerAndBar from "../Layout/DrawerAndBar";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function RootLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <>
       <main>
